a2309bf0ef534d8f748b5f2b4943f650fb811fa9
Raj Dhadda <raj.dhadda@trescal.com>
2024-05-21 13:32:42 +0000

    feat: routed homepage to tasks.html

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+
 if os.path.exists("env.py"):
     import env  # noqa
 
@@ -8,6 +9,7 @@ if os.path.exists("env.py"):
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+
 
 db = SQLAlchemy(app)
 
